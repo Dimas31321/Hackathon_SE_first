f8b87e98a48cd8f7fa58587da7193dc06599ef85
Dmitry <dl030507@gmail.com>
2026-05-29 23:24:25 +0300

    Добавил в init к классу информацию о категории письма: получено или отправлено

--- src/Email.py
+++ src/Email.py
@@ -1,14 +1,15 @@
-
 class Email:
     def __init__(self, sender: str, 
                  recipient: str, 
                  theme: str, 
                  filename: str,
+                 sent_or_received: str,
                  body: str = "",
                  category: str = None):
         self.sender = sender
         self.recipient = recipient
         self.theme = theme
+        self.sent_or_received = sent_or_received
         self.filename = filename
         self.body = body
         self.category = category
@@ -18,7 +19,8 @@ class Email:
                f"Theme: {self.theme}\n" \
                f"Filename: {self.filename}\n" \
                f"Body: {self.body}\n" \
-               f"Category: {self.category}"
+               f"Category: {self.category}\n" \
+               f"Sent or Received: {self.sent_or_received}"
     def get_filename(self):
         return self.filename
     def get_body(self):
@@ -31,3 +33,7 @@ class Email:
         return self.theme
     def get_category(self):
         return self.category
+    def set_category(self, category: str):
+        self.category = category
+    def get_sent_or_received(self):
+        return self.sent_or_received
