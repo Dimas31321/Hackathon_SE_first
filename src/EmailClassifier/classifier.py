167e19ce7cbe42ff90631b632ef6dd7375c5c73c
ubmt2 <alexdemin1607@gmail.com>
2026-05-30 15:24:46 +0300

    Сортировка по ключевым словам и определитель

---
+++ src/EmailClassifier/classifier.py
@@ -0,0 +1,23 @@
+from typing import List
+
+from src.Email import Email
+from src.EmailClassifier.Rules.rule import Rule
+
+
+class Classifier:
+    def __init__(self, rules: List[Rule]):
+        self.rules = rules
+
+    def classify(self, email: Email):
+        finalScore = dict()
+
+        for rule in self.rules:
+            score = rule.score(email)
+
+            for cat, value in score.items(): #пройтись по всем результатам и их суммировать
+                if cat in finalScore:
+                    finalScore[cat] += value
+                else:
+                    finalScore[cat] = value
+        
+        return max(finalScore.items(), key= lambda x: x[1])[0]
