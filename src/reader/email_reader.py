acc92d1244861aec8864a2e4e14bbe7b9d8a97be
Dmitry <dl030507@gmail.com>
2026-05-30 15:17:03 +0300

    Большой апдейт. Глобально изменил логику. Теперь принимает .txt и .json. Для остальных форматов пока падает

--- src/reader/email_reader.py
+++ src/reader/email_reader.py
@@ -6,48 +6,111 @@ class EmailReader:
     def __init__(self, filename: str):
         self.filename = filename # Filename следует вводить с раширением, например "email.txt"
         self.path = Path(filename)
-    def read_email(self) -> Email:
-        if (self.filename.endswith('.txt') or self.filename.endswith('.bin')):
-            try:
-                with open(self.path, 'r') as file:
-                    lines = file.readlines()
-            except FileNotFoundError:
-                print(f"Файл '{self.filename}' не найден.")
-                return None
-            except PermissionError:
-                print(f"Нет доступа к файлу '{self.filename}'.")
-                return None
-            except IsADirectoryError:
-                print(f"'{self.filename}' является директорией, а не файлом.")
-                return None
-            except UnicodeDecodeError:
-                print(f"Ошибка декодирования файла '{self.filename}'.")
-                return None
-            except Exception as e:
-                print(f"Произошла ошибка при чтении файла '{self.filename}': {e}.")
-                return None
-        elif self.filename.endswith('.json'):
-            try:
-                with open(self.path, 'r') as file:
-                    lines = json.load(file)
-            except FileNotFoundError:
-                print(f"Файл '{self.filename}' не найден.")
-                return None
-            except PermissionError:
-                print(f"Нет доступа к файлу '{self.filename}'.")
-                return None
-            except IsADirectoryError:
-                print(f"'{self.filename}' является директорией, а не файлом.")
-                return None
-            except UnicodeDecodeError:
-                print(f"Ошибка декодирования файла '{self.filename}'.")
-                return None
-            except Exception as e:
-                print(f"Произошла ошибка при чтении файла '{self.filename}': {e}.")
-                return None
-        else:
-            print(f"Неподдерживаемый формат файла '{self.filename}'. Поддерживаются только .txt и .json.")
+    def read_email(self) -> Email | None:
+        if not self.path.exists():
+            print(f"Файл '{self.filename}' не найден.")
             return None
+        
+        if self.path.is_dir():
+            print(f"'{self.filename}' является директорией, а не файлом.")
+            return None
+        if self.filename.endswith(".txt"):
+            data = self._read_txt_file()
+        elif self.filename.endswith(".json"):
+            data = self._read_json_file()
+        else:
+            print(
+                f"Неподдерживаемый формат файла '{self.filename}'. "
+                f"Поддерживаются только .txt и .json."
+            )
+            return None
+        if data is None or len(data) == 0:
+            return None
+        else:
+            return self._build_email(data)
+        
+    def _read_txt_file(self) -> list[str] | None:
+        try:
+            with open(self.path, 'r') as file:
+                return file.readlines()
+                
+        except FileNotFoundError:
+            print(f"Файл '{self.filename}' не найден.")
+            return None
+        
+        except PermissionError:
+            print(f"Нет доступа к файлу '{self.filename}'.")
+            return None
+        
+        except IsADirectoryError:
+            print(f"'{self.filename}' является директорией, а не файлом.")
+            return None
+        
+        except UnicodeDecodeError:
+            print(f"Ошибка декодирования файла '{self.filename}'.")
+            return None
+        
+        except Exception as e:
+            print(f"Произошла ошибка при чтении файла '{self.filename}': {e}.")
+            return None
+            
+    def _read_json_file(self) -> None | list[str] | dict:
+        try:
+            with open(self.path, 'r') as file:
+                data = json.load(file)
+            if isinstance(data, list):
+                return data
+            
+            if isinstance(data, dict):
+                lines = []
+                if "From" in data:
+                    lines.append(f"From: {data['From']}")
+                elif "От кого" in data:
+                    lines.append(f"От кого: {data['От кого']}")
+                if "To" in data:
+                    lines.append(f"To: {data['To']}")
+                elif "Кому" in data:
+                    lines.append(f"Кому: {data['Кому']}")
+                if "Subject" in data:
+                    lines.append(f"Subject: {data['Subject']}")
+                elif "Тема" in data:
+                    lines.append(f"Тема: {data['Тема']}")
+                if "Date" in data:
+                    lines.append(f"Date: {data['Date']}")
+                elif "Дата" in data:
+                    lines.append(f"Дата: {data['Дата']}")
+                if "body" in data:
+                    lines.append(data["body"])
+                elif "text" in data: # Добавить аналоги на русском
+                    lines.append(data["text"])
+
+                return lines
+            
+            print(f"Некорректная структура JSON в файле '{self.filename}'.")
+            return None
+            # TODO: Проверить на наличие других полей, которые могут содержать тело письма
+                
+        except FileNotFoundError:
+            print(f"Файл '{self.filename}' не найден.")
+            return None
+        
+        except PermissionError:
+            print(f"Нет доступа к файлу '{self.filename}'.")
+            return None
+        
+        except IsADirectoryError:
+            print(f"'{self.filename}' является директорией, а не файлом.")
+            return None
+        
+        except UnicodeDecodeError:
+            print(f"Ошибка декодирования файла '{self.filename}'.")
+            return None
+        
+        except Exception as e:
+            print(f"Произошла ошибка при чтении файла '{self.filename}': {e}.")
+            return None
+    
+    def _build_email(self, lines: list[str]) -> Email:
         sender: str =""
         recipient: str = ""
         theme: str = ""
@@ -74,12 +137,13 @@ class EmailReader:
                   or clean_line.startswith("Дата:")) and not date:
                 date = clean_line[5:].strip() 
             else:
-                body = body + line + "\n"
-        return Email(sender, 
-                     recipient, 
-                     theme, 
-                     date, # Дата может быть в разных форматах, сложно распарсить
-                     self.filename,
-                     None, # Неизвестно, отправлено или получено
-                     body)
+                body += line
+        return Email(
+                        sender,
+                        recipient,
+                        theme,
+                        date,
+                        self.filename,
+                        body
+                    )
 
