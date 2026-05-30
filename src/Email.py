53995d5943af75ec47e5bda7c5c7bcf47fe98f5e
Dmitry <dl030507@gmail.com>
2026-05-30 13:18:20 +0300

    Убрал геттеры и добавил date

--- src/Email.py
+++ src/Email.py
@@ -2,14 +2,14 @@ class Email:
     def __init__(self, sender: str, 
                  recipient: str, 
                  theme: str, 
+                 data: str,
                  filename: str,
-                 sent_or_received: str,
                  body: str = "",
                  category: str = None):
         self.sender = sender
         self.recipient = recipient
         self.theme = theme
-        self.sent_or_received = sent_or_received
+        self.data = data
         self.filename = filename
         self.body = body
         self.category = category
@@ -20,20 +20,6 @@ class Email:
                f"Filename: {self.filename}\n" \
                f"Body: {self.body}\n" \
                f"Category: {self.category}\n" \
-               f"Sent or Received: {self.sent_or_received}"
-    def get_filename(self):
-        return self.filename
-    def get_body(self):
-        return self.body
-    def get_sender(self):
-        return self.sender
-    def get_recipent(self):
-        return self.recipient
-    def get_theme(self):
-        return self.theme
-    def get_category(self):
-        return self.category
-    def set_category(self, category: str):
-        self.category = category
-    def get_sent_or_received(self):
-        return self.sent_or_received
+               f"Data: {self.data}"
+               
+# от кого кому тема дата тело название 
