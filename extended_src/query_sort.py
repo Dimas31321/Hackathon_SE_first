4daf7f9816d3e6035631de99e87139ed05c43fca
stasssokolovskiy <stasssokolovskiy@gmail.com>
2026-06-02 00:02:11 +0300

    Ветка для докера

---
+++ extended_src/query_sort.py
@@ -0,0 +1,35 @@
+import os
+os.environ["HF_HUB_OFFLINE"] = "1"
+
+from fastembed import TextEmbedding
+import logging
+from src.email import Email
+from sklearn.metrics.pairwise import cosine_similarity 
+
+logging.getLogger("fastembed").setLevel(logging.WARNING)
+
+try:
+    model = TextEmbedding(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+except:
+    error_text = "Не удалось загрузить модель"
+    logging.error(error_text)
+    raise FileNotFoundError(error_text)
+
+class Query_sorter:
+    def __init__(emails:list[Email]):
+        self.emails = emails
+        email_texts = []
+
+        for email in emails:
+            email_texts.append(f"{email.theme}\n{email.body}")
+
+        self.email_embeddings = list(model.embed(email_texts))
+
+    def query(query:str) -> list[Email]:
+        query_embedding = list(model.embed([query]))
+
+        cos_similarities = cosine_similarity(query_embedding, self.email_embeddings).flatten()
+
+        out = sorted(zip(cos_similarities, emails), key=lambda x: x[0])
+        return [email for cos, email in out]
+
---
+++ src/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+[ -f "./extended_src/query_sort.py" ] && [ -f "./extended_src/requirements.txt" ]
+NO_EXTENDED_VERSION=$?
+python -m src.main
