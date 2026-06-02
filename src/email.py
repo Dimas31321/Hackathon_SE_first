2a4f0e9ec97a10b9a56ae0acb5cac1d330b569ab
stasssokolovskiy <stasssokolovskiy@gmail.com>
2026-06-02 03:49:14 +0300

    Reapply "Добавил остальное"
    This reverts commit 588db8fe348841d3ada6cb81190607dd04e1d836.

---
+++ src/email.py
@@ -0,0 +1,30 @@
+class Email:
+    def __init__(self, sender: str, 
+                 recipient: str, 
+                 theme: str, 
+                 date: str,
+                 filename: str,
+                 body: str = "",
+                 categories: list[str] | None = None):
+        self.sender = sender
+        self.recipient = recipient
+        self.theme = theme
+        self.date = date
+        self.filename = filename
+        self.body = body
+        self.categories = categories if categories is not None else []
+
+    def __str__(self):
+        return f"From: {self.sender}\n" \
+               f"To: {self.recipient}\n" \
+               f"Theme: {self.theme}\n" \
+               f"Filename: {self.filename}\n" \
+               f"Body: {self.body}\n" \
+               f"Categories: {self.categories}\n" \
+               f"Date: {self.date}"
+    def add_category(self, category: str) -> bool:
+        if category not in self.categories:
+            self.categories.append(category)
+            return True
+        return False
+
